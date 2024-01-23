faa642a81f2c72f7dad7be89be4416fb37ddaad7
arun.akds@gmail.com <arun.akds@gmail.com>
2024-01-23 09:11:27 +0530

    fix : Removed Iwex Test Doctype

--- iwapp_com/iwex_apps/doctype/iwex_test/iwex_test.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2024, Iwex and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on("Iwex Test", {
-	refresh(frm) {
-        frappe.msgprint("jjjjjjjjjjjjjj")
-	},
-});
