14292f17097cae7eb36c642eec59a07992211f27
arun.akds@gmail.com <arun.akds@gmail.com>
2024-02-14 00:42:08 +0530

    fix

--- iwapp_com/iwex_apps/doctype/opportunity_source/opportunity_source.js
+++ iwapp_com/iwex_apps/doctype/opportunity_source/opportunity_source.js
@@ -1,8 +1,218 @@
 // Copyright (c) 2024, iWEX and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Opportunity Source", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Opportunity Source", {
+	check(frm) {
+        frm.clear_table("details")
+        frm.refresh_fields("details");
+        console.log("hiiiiiiiiii")
+        if (frm.doc.first_name && !frm.doc.email && !frm.doc.mobile_no){
+            // frm.set_value("organisation_name", "")
+            console.log("first")
+            frappe.db.get_list('Customer', {
+                fields: ['name', 'custom_email',
+                'custom_first_name', 'custom_mobile_no', 'custom_organisation_name'],
+                filters: {
+                    "name": ["like", "%" + frm.doc.first_name + "%"],
+                },
+            }).then(records => {
+                console.log(records);
+                if (records.length > 0){
+                    console.log("custttt")
+                    $.each(records, function(i, cust){
+                        var child = cur_frm.add_child("details");
+                        child.email= cust.custom_email
+                        child.id = cust.name
+                        child.document = "Customer"
+                        child.first_name = cust.custom_first_name
+                        child.mobile_no = cust.custom_mobile_no
+                        child.organisation_name = cust.custom_organisation_name
+                        frm.refresh_fields("details");
+                    })
+                }
+                else{
+                    console.log("lead")
+                    frappe.db.get_list('Lead', {
+                        fields: ['name', 'email_id',
+                        'first_name', 'mobile_no', 'company_name'],
+                        filters: {
+                            "lead_name": ["like", "%" + frm.doc.first_name + "%"],
+                        },
+                    }).then(records => {
+                        console.log(records);
+                        if (records){
+                            $.each(records, function(i, lead){
+                                var child = cur_frm.add_child("details");
+                                child.email= lead.email
+                                child.id = lead.name
+                                child.document = "Lead"
+                                child.first_name = lead.first_name
+                                child.mobile_no = lead.mobile_no
+                                child.organisation_name = lead.company_name
+                                frm.refresh_fields("details");
+                            })
+                        }
+                    })
+                }
+            })
+        }
+        if (frm.doc.organisation_name && !frm.doc.email && !frm.doc.mobile_no){
+            console.log("first")
+            // frm.set_value("first_name", "")
+            frappe.db.get_list('Customer', {
+                fields: ['name', 'custom_email',
+                'custom_first_name', 'custom_mobile_no', 'custom_organisation_name'],
+                filters: {
+                    "custom_organisation_name": ["like", "%" + frm.doc.organisation_name + "%"],
+                },
+            }).then(records => {
+                console.log(records);
+                if (records.length > 0){
+                    console.log("custttt")
+                    $.each(records, function(i, cust){
+                        var child = cur_frm.add_child("details");
+                        child.email= cust.custom_email
+                        child.id = cust.name
+                        child.document = "Customer"
+                        child.first_name = cust.custom_first_name
+                        child.mobile_no = cust.custom_mobile_no
+                        child.organisation_name = cust.custom_organisation_name
+                        frm.refresh_fields("details");
+                    })
+                }
+                else{
+                    console.log("lead")
+                    frappe.db.get_list('Lead', {
+                        fields: ['name', 'email_id',
+                        'first_name', 'mobile_no', 'company_name'],
+                        filters: {
+                            "company_name": ["like", "%" + frm.doc.organisation_name + "%"],
+                        },
+                    }).then(records => {
+                        console.log(records);
+                        if (records){
+                            $.each(records, function(i, lead){
+                                var child = cur_frm.add_child("details");
+                                child.email= lead.email_id
+                                child.id = lead.name
+                                child.document = "Lead"
+                                child.first_name = lead.first_name
+                                child.mobile_no = lead.mobile_no
+                                child.organisation_name = lead.company_name
+                                frm.refresh_fields("details");
+                            })
+                        }
+                    })
+                }
+            })
+        }
+        if (frm.doc.organisation_name && frm.doc.email && frm.doc.mobile_no){
+            console.log("full with org")
+            // frm.set_value("first_name", "")
+            frappe.db.get_list('Customer', {
+                fields: ['name', 'custom_email',
+                'custom_first_name', 'custom_mobile_no', 'custom_organisation_name'],
+                filters: {
+                    "custom_organisation_name": ["like", "%" + frm.doc.organisation_name + "%"],
+                    "custom_email": ["=", frm.doc.email],
+                    "custom_mobile_no":["=", frm.doc.mobile_no]
+                },
+            }).then(records => {
+                console.log(records);
+                if (records.length > 0){
+                    console.log("custttt")
+                    $.each(records, function(i, cust){
+                        var child = cur_frm.add_child("details");
+                        child.email= cust.custom_email
+                        child.id = cust.name
+                        child.document = "Customer"
+                        child.first_name = cust.custom_first_name
+                        child.mobile_no = cust.custom_mobile_no
+                        child.organisation_name = cust.custom_organisation_name
+                        frm.refresh_fields("details");
+                    })
+                }
+                else{
+                    console.log("lead")
+                    frappe.db.get_list('Lead', {
+                        fields: ['name', 'email_id',
+                        'first_name', 'mobile_no', 'company_name'],
+                        filters: {
+                            "company_name": ["like", "%" + frm.doc.organisation_name + "%"],
+                            "email_id": ["=", frm.doc.email],
+                            "mobile_no":["=", frm.doc.mobile_no]
+                        },
+                    }).then(records => {
+                        console.log(records);
+                        if (records){
+                            $.each(records, function(i, lead){
+                                var child = cur_frm.add_child("details");
+                                child.email= lead.email_id
+                                child.id = lead.name
+                                child.document = "Lead"
+                                child.first_name = lead.first_name
+                                child.mobile_no = lead.mobile_no
+                                child.organisation_name = lead.company_name
+                                frm.refresh_fields("details");
+                            })
+                        }
+                    })
+                }
+            })
+        }
+        if (frm.doc.first_name && frm.doc.email && frm.doc.mobile_no){
+            console.log("full with org")
+            // frm.set_value("first_name", "")
+            frappe.db.get_list('Customer', {
+                fields: ['name', 'custom_email',
+                'custom_first_name', 'custom_mobile_no', 'custom_organisation_name'],
+                filters: {
+                    "name": ["like", "%" + frm.doc.first_name + "%"],
+                    "custom_email": ["=", frm.doc.email],
+                    "custom_mobile_no":["=", frm.doc.mobile_no]
+                },
+            }).then(records => {
+                console.log(records);
+                if (records.length > 0){
+                    console.log("custttt")
+                    $.each(records, function(i, cust){
+                        var child = cur_frm.add_child("details");
+                        child.email= cust.custom_email
+                        child.id = cust.name
+                        child.document = "Customer"
+                        child.first_name = cust.custom_first_name
+                        child.mobile_no = cust.custom_mobile_no
+                        child.organisation_name = cust.custom_organisation_name
+                        frm.refresh_fields("details");
+                    })
+                }
+                else{
+                    console.log("lead")
+                    frappe.db.get_list('Lead', {
+                        fields: ['name', 'email_id',
+                        'first_name', 'mobile_no', 'company_name'],
+                        filters: {
+                            "lead_name": ["like", "%" + frm.doc.first_name + "%"],
+                            "email_id": ["=", frm.doc.email],
+                            "mobile_no":["=", frm.doc.mobile_no]
+                        },
+                    }).then(records => {
+                        console.log(records);
+                        if (records){
+                            $.each(records, function(i, lead){
+                                var child = cur_frm.add_child("details");
+                                child.email= lead.email_id
+                                child.id = lead.name
+                                child.document = "Lead"
+                                child.first_name = lead.first_name
+                                child.mobile_no = lead.mobile_no
+                                child.organisation_name = lead.company_name
+                                frm.refresh_fields("details");
+                            })
+                        }
+                    })
+                }
+            })
+        }
+	},
+});
--- iwapp_com/iwex_apps/doctype/opportunity_source/opportunity_source.py
+++ iwapp_com/iwex_apps/doctype/opportunity_source/opportunity_source.py
@@ -1,5 +1,5 @@
-# Copyright (c) 2024, iWEX and contributors
-# For license information, please see license.txt
+# # Copyright (c) 2024, iWEX and contributors
+# # For license information, please see license.txt
 import re
 import frappe
 from frappe.model.document import Document
@@ -7,107 +7,107 @@ from frappe.model.document import Document
 
 class OpportunitySource(Document):
 	pass
-	def validate(self):
-		mob_no = frappe.db.get_list('Contact',
-			filters={
-				'custom_mob': ['=', ''],
-				'custom_organisation_name':['=', '']
-			},
-			fields=['company_name', 'mobile_no', 'name'],
-			as_list=False
-		)
-		cleaned_organization_name = remove_suffixes_from_field(self.organisation_name)
-		for mob in mob_no:
-			mob_10_digits = mob.get('mobile_no')[-10:]
-			if mob.get('company_name') != None:
-				org_name = remove_suffixes_from_field(mob.get('company_name'))
-			if mob_10_digits and org_name:
-				frappe.db.set_value("Contact", mob.get('name'), {"custom_mob": mob_10_digits, "custom_organisation_name":org_name}, update_modified=False)
-		last_10_digits = self.mobile_no[-10:]
-		opp_organisation_exists = frappe.db.exists("Contact", {"email_id": self.email, "custom_mob": last_10_digits, "custom_organisation_name":cleaned_organization_name})
-		if opp_organisation_exists:
-			organisation = frappe.db.get_all('Dynamic Link',
-				filters={
-					'parent': opp_organisation_exists
-				},
-				fields=['link_doctype', 'link_name'],
-				as_list=False
-			)
-			if organisation:
-				if len(organisation) > 1:
-					for i in organisation:
-						if i.get('link_doctype') == "Customer":
-							self.party_type = i.get('link_doctype')
-							self.party_name = i.get('link_name')
-				if len(organisation) == 1:
-					for i in organisation:
-						if i.get('link_doctype') == "Customer":
-							self.party_type = i.get('link_doctype')
-							self.party_name = i.get('link_name')
-						else:
-							self.party_type = i.get('link_doctype')
-							self.party_name = i.get('link_name')
-		if not opp_organisation_exists:
-			self.party_type = ""
-			self.party_name = ""
-			opp_firstname_exists = frappe.db.exists("Contact", {"email_id": self.email, "custom_mob": last_10_digits, "first_name":self.first_name})
-			if opp_firstname_exists:
-				contact = frappe.get_doc("Contact", opp_firstname_exists)
-				if contact.middle_name == self.middle_name or contact.middle_name == self.last_name:
-					firstname = frappe.db.get_all('Dynamic Link',
-					filters={
-						'parent': opp_firstname_exists
-					},
-					fields=['link_doctype', 'link_name'],
-					as_list=False
-					)
-					if firstname:
-						if len(firstname) > 1:
-							for i in firstname:
-								if i.get('link_doctype') == "Customer":
-									self.party_type = i.get('link_doctype')
-									self.party_name = i.get('link_name')
-						if len(firstname) == 1:
-							for i in firstname:
-								if i.get('link_doctype') == "Customer":
-									self.party_type = i.get('link_doctype')
-									self.party_name = i.get('link_name')
-								else:
-									self.party_type = i.get('link_doctype')
-									self.party_name = i.get('link_name')
-				else:
-					firstname = frappe.db.get_all('Dynamic Link',
-					filters={
-						'parent': opp_firstname_exists
-					},
-					fields=['link_doctype', 'link_name'],
-					as_list=False
-					)
-					if firstname:
-						if len(firstname) > 1:
-							for i in firstname:
-								if i.get('link_doctype') == "Customer":
-									self.party_type = i.get('link_doctype')
-									self.party_name = i.get('link_name')
-						if len(firstname) == 1:
-							for i in firstname:
-								if i.get('link_doctype') == "Customer":
-									self.party_type = i.get('link_doctype')
-									self.party_name = i.get('link_name')
-								else:
-									self.party_type = i.get('link_doctype')
-									self.party_name = i.get('link_name')
+# 	def validate(self):
+# 		mob_no = frappe.db.get_list('Contact',
+# 			filters={
+# 				'custom_mob': ['=', ''],
+# 				'custom_organisation_name':['=', '']
+# 			},
+# 			fields=['company_name', 'mobile_no', 'name'],
+# 			as_list=False
+# 		)
+# 		cleaned_organization_name = remove_suffixes_from_field(self.organisation_name)
+# 		for mob in mob_no:
+# 			mob_10_digits = mob.get('mobile_no')[-10:]
+# 			if mob.get('company_name') != None:
+# 				org_name = remove_suffixes_from_field(mob.get('company_name'))
+# 			if mob_10_digits and org_name:
+# 				frappe.db.set_value("Contact", mob.get('name'), {"custom_mob": mob_10_digits, "custom_organisation_name":org_name}, update_modified=False)
+# 		last_10_digits = self.mobile_no[-10:]
+# 		opp_organisation_exists = frappe.db.exists("Contact", {"email_id": self.email, "custom_mob": last_10_digits, "custom_organisation_name":cleaned_organization_name})
+# 		if opp_organisation_exists:
+# 			organisation = frappe.db.get_all('Dynamic Link',
+# 				filters={
+# 					'parent': opp_organisation_exists
+# 				},
+# 				fields=['link_doctype', 'link_name'],
+# 				as_list=False
+# 			)
+# 			if organisation:
+# 				if len(organisation) > 1:
+# 					for i in organisation:
+# 						if i.get('link_doctype') == "Customer":
+# 							self.party_type = i.get('link_doctype')
+# 							self.party_name = i.get('link_name')
+# 				if len(organisation) == 1:
+# 					for i in organisation:
+# 						if i.get('link_doctype') == "Customer":
+# 							self.party_type = i.get('link_doctype')
+# 							self.party_name = i.get('link_name')
+# 						else:
+# 							self.party_type = i.get('link_doctype')
+# 							self.party_name = i.get('link_name')
+# 		if not opp_organisation_exists:
+# 			self.party_type = ""
+# 			self.party_name = ""
+# 			opp_firstname_exists = frappe.db.exists("Contact", {"email_id": self.email, "custom_mob": last_10_digits, "first_name":self.first_name})
+# 			if opp_firstname_exists:
+# 				contact = frappe.get_doc("Contact", opp_firstname_exists)
+# 				if contact.middle_name == self.middle_name or contact.middle_name == self.last_name:
+# 					firstname = frappe.db.get_all('Dynamic Link',
+# 					filters={
+# 						'parent': opp_firstname_exists
+# 					},
+# 					fields=['link_doctype', 'link_name'],
+# 					as_list=False
+# 					)
+# 					if firstname:
+# 						if len(firstname) > 1:
+# 							for i in firstname:
+# 								if i.get('link_doctype') == "Customer":
+# 									self.party_type = i.get('link_doctype')
+# 									self.party_name = i.get('link_name')
+# 						if len(firstname) == 1:
+# 							for i in firstname:
+# 								if i.get('link_doctype') == "Customer":
+# 									self.party_type = i.get('link_doctype')
+# 									self.party_name = i.get('link_name')
+# 								else:
+# 									self.party_type = i.get('link_doctype')
+# 									self.party_name = i.get('link_name')
+# 				else:
+# 					firstname = frappe.db.get_all('Dynamic Link',
+# 					filters={
+# 						'parent': opp_firstname_exists
+# 					},
+# 					fields=['link_doctype', 'link_name'],
+# 					as_list=False
+# 					)
+# 					if firstname:
+# 						if len(firstname) > 1:
+# 							for i in firstname:
+# 								if i.get('link_doctype') == "Customer":
+# 									self.party_type = i.get('link_doctype')
+# 									self.party_name = i.get('link_name')
+# 						if len(firstname) == 1:
+# 							for i in firstname:
+# 								if i.get('link_doctype') == "Customer":
+# 									self.party_type = i.get('link_doctype')
+# 									self.party_name = i.get('link_name')
+# 								else:
+# 									self.party_type = i.get('link_doctype')
+# 									self.party_name = i.get('link_name')
 
 
-def remove_suffixes_from_field(field_value, suffixes=None):
-    if suffixes is None:
-        suffixes = ["Pvt Ltd", "Ltd", "LLP", "Co Ltd"]
+# def remove_suffixes_from_field(field_value, suffixes=None):
+#     if suffixes is None:
+#         suffixes = ["Pvt Ltd", "Ltd", "LLP", "Co Ltd"]
 
-    # Create a regular expression pattern for matching suffixes
-    pattern = r"\s*({})\s*$".format("|".join(map(re.escape, suffixes)))
+#     # Create a regular expression pattern for matching suffixes
+#     pattern = r"\s*({})\s*$".format("|".join(map(re.escape, suffixes)))
 
-    # Remove matching suffixes from the field value
-    cleaned_value = re.sub(pattern, "", field_value)
+#     # Remove matching suffixes from the field value
+#     cleaned_value = re.sub(pattern, "", field_value)
 
-    return cleaned_value
+#     return cleaned_value
 
